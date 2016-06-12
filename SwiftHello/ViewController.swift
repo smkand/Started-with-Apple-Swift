feb4615c5e7eaef40fd2547d091b236df53709eb
Shobha Kand <shobha020@gmail.com>
2016-06-12 18:15:13 -0400

    Initial commit

---
+++ SwiftHello/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  SwiftHello
+//
+//  Created by Shobha Kand on 6/12/16.
+//  Copyright © 2016 theswiftproject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+   
+    @IBOutlet weak var enterTextBox: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myLabel.text = "Hello World"
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pressButton() {
+        myLabel.text = enterTextBox.text
+        
+    }
+
+}
+
